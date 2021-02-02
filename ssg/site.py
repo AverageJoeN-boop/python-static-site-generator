89d81a203cdf36aaac5e094c44faeba59fbf74f7
joe110518 <JNetzel@paycor.com>
2021-02-02 15:35:16 -0500

    Update site.py

--- ssg/site.py
+++ ssg/site.py
@@ -16,6 +16,8 @@ class Site:
         for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
+            elif path.is_file():
+                self.run_parser(path)
 
     def load_parser(self, extension):
         for parser in self.parsers:
@@ -24,4 +26,8 @@ class Site:
 
     def run_parser(self, path):
         parser = self.load_parser(path.suffix)
-        return parser
+
+        if parser is not None:
+            parser.parse(path, self.source, self.dest)
+        else:
+            print("Not Implemented")
