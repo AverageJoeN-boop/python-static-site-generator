a09e39b8116783c1a2727f9d89f690f177f2223a
joe110518 <JNetzel@paycor.com>
2021-02-02 15:28:28 -0500

    Update site.py

--- ssg/site.py
+++ ssg/site.py
@@ -16,3 +16,12 @@ class Site:
         for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
+
+    def load_parser(self, extension):
+        for parser in self.parsers:
+            if parser.valid_extension(extension):
+                return parser
+
+    def run_parser(self, path):
+        parser = self.load_parser(path.suffix)
+        return parser
